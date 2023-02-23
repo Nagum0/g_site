75c6f1c0aa347375001ad06ccad0ea124816449f
zeki <121572632+Nagum0@users.noreply.github.com>
2023-02-23 17:15:45 +0100

    Add files via upload

--- blackjack.js
+++ blackjack.js
@@ -2,8 +2,44 @@ class BlackJack {
 	constructor () {
 		console.log("hello blackjack");
 	}
+
+	//BET MENU METHODS
+	showBetMenu(betMenu) {
+		betMenu.classList.remove("hidden");
+		betMenu.classList.add("bet_menu");
+	}
+
+	bettingBios(biosNum, betBios, biosInput, errMsg, betMenu) {
+		if (biosInput.value > biosNum) {
+			console.log("You don't have enought BIOS");
+			errMsg.classList.remove("hidden");
+		} else {
+			betBios = biosInput.value;
+			biosNum -= betBios;
+			betMenu.style.display = "none";
+
+			console.log(`Bet BIOS: ${betBios}`);
+			console.log(`Player BIOS: ${biosNum}`);
+		}
+	}
+
 }
+
+//GLOBAL VARIABLES
+main = new BlackJack();
+let playerBios = 15;
+let enemyBios = 15;
+let playerBetBios = 0;
+let enemyBetBios = 0;
 
 const dealerCont = document.getElementById("dealerCont");
 
-main = new BlackJack();
+//SHOW BET MENU
+const betBtn = document.getElementById("betBtn");
+const betMenu = document.getElementById("betMenu");
+const betInput = document.getElementById("betInput");
+const biosErrorLbl = document.getElementById("biosErrorLbl");
+const confirmBetBtn = document.getElementById("confirmBetBtn");
+
+betBtn.addEventListener("click", () => {main.showBetMenu(betMenu)});
+confirmBetBtn.addEventListener("click", () => {main.bettingBios(playerBios, playerBetBios, betInput, biosErrorLbl, betMenu)});
